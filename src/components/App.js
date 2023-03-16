e48873c6b331f7f32840b2819ae6d22469328d69
sangitasarkar74 <sangitasarkar@gmail.com>
2023-03-16 20:03:03 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,58 @@
-import React, { useState } from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
-function ToDo() {
-  return (<tr>
-    <td>
-      <p>id</p>
-    </td>
-    <td>
-      <input />
-    </td>
-    <td>
-      <p>createdAt</p>
-    </td>
-  </tr>)
+function ToDo({ id, createdAt, todos, handleSubmit }) {
+  return (
+    <>
+      {todos.map((todo) => {
+        return (
+          <tr>
+            <td>
+              <p>{todo.id}</p>
+            </td>
+            <td>
+              <input type="text" />
+            </td>
+            <td>
+              <p>{todo.createdAt}</p>
+            </td>
+          </tr>
+        );
+      })}
+    </>
+  );
 }
 
 function App() {
-  const [todos, setTodos] = useState([{
-    id: 'todo1',
-    createdAt: '20:30',
-  }, {
-    id: 'todo2',
-    createdAt: '18:00',
-  }
+  const [todos, setTodos] = useState([
+    {
+      id: "todo1",
+      createdAt: "20:30",
+    },
+    {
+      id: "todo2",
+      createdAt: "18:00",
+    },
   ]);
+  const handleSubmit = () => {
+    setTodos(todos);
+  };
 
   return (
     <div id="main">
-      <button>Reverse</button>
+      <button onClick={handleSubmit}>Reverse</button>
       <table>
         <tbody>
+          <ToDo
+            todoId={id}
+            createdAt={createdAt}
+            todos={todos}
+            handleSubmit={handleSubmit}
+          />
         </tbody>
       </table>
     </div>
-  )
+  );
 }
-
 
 export default App;
